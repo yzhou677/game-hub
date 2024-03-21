8bc22bf51cb5db894ad4987e172edc641178a21c
Yuqi Zhou <yzhou677@gmail.com>
2024-03-21 15:05:42 -0500

    Displaying critic score

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -12,7 +12,8 @@ export interface Game {
     name: string;
     id: number;
     background_image: string;
-    parent_platforms: { platform: Platform }[]
+    parent_platforms: { platform: Platform }[];
+    metacritic: number;
   }
   
   interface FetchGamesResponse {
