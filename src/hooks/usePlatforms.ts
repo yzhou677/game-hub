a8ed2d897ae93bc925c9cd4701b35d6da74c0f69
Yuqi Zhou <yzhou677@gmail.com>
2024-03-29 18:25:06 -0500

    Refactor-create lookup hooks

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -14,7 +14,7 @@ const usePlatforms = () => useQuery({
     queryKey: ['platforms'],
     queryFn: apiClient.getAll,
     staleTime: 24 * 60 * 60 * 1000, //24h
-    initialData: { count: platforms.count, next: null, results: platforms.results }
+    initialData: platforms
 })
 
 export default usePlatforms;
