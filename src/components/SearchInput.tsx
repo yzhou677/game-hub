a7d9b780e8d0ba34bfc9c1e5ea4a653f4384bb69
Yuqi Zhou <yzhou677@gmail.com>
2025-11-12 22:05:00 -0600

    Fixed search feature bug: navigate to home instead of games list

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,8 +1,8 @@
 import { Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
 import { useRef } from "react";
 import { BsSearch } from "react-icons/bs";
-import useGameQueryStore from "../store";
 import { useNavigate } from "react-router-dom";
+import useGameQueryStore from "../store";
 
 const SearchInput = () => {
   const ref = useRef<HTMLInputElement>(null);
@@ -14,7 +14,7 @@ const SearchInput = () => {
       onSubmit={(event) => {
         event.preventDefault();
         if (ref.current) setSearchText(ref.current.value);
-        navigate("/");
+        navigate("/games");
       }}
     >
       <InputGroup>
